585cab315329066a4987a06a14e42f8dea03bb68
showkatbinidris70 <showkatbinidris70@gmail.com>
2023-04-01 15:24:58 +0600

    video no 15

--- figma-to-rj/src/pages/components/Footer.js
+++ figma-to-rj/src/pages/components/Footer.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import {FooterSection} from '../../styles/Navbar.styled'
+import React from "react";
+import { FooterSection } from "../../styles/Navbar.styled";
 
 export default function Footer() {
-    return (<FooterSection>
-        <h2>All credit goes to
-            <span>
-                CODE  ABC
-            </span>
-        </h2>
-    </FooterSection>)
+  return (
+    <FooterSection>
+      <h2>
+        All credit goes to <span> Showk@t Ali </span>
+      </h2>
+    </FooterSection>
+  );
 }
