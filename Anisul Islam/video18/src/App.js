7c41aecec46d05121d3f386b65f5227b9bf2deeb
showkatbinidris70 <showkatbinidris70@gmail.com>
2023-04-03 16:43:57 +0600

    video 17-20

---
+++ Anisul Islam/video18/src/App.js
@@ -0,0 +1,17 @@
+import { FaHeart } from "react-icons/fa";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>
+        Hello, Showk@t. I{" "}
+        <span>
+          <FaHeart className="heart-icon" />
+        </span> Bangladesh.
+      </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ Anisul Islam/video20-state-event/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import State from "./State";
+
+function App() {
+  return (
+    <div>
+      <State count="0" />
+    </div>
+  );
+}
+
+export default App;
